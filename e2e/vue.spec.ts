f2f172a4862bad239741944d4ae9725f0ecb22aa
Joel <raizetvision@gmail.com>
2025-05-23 16:35:20 +0200

    add e2e tests ci

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
+  await page.setViewportSize({ width: 1280, height: 800 });
   await page.goto('/');
-  await expect(page.locator('h1')).toHaveText('You did it!');
+  await expect(page.locator('h1')).toHaveText('Trouvez votre prochain chez vous');
 })
