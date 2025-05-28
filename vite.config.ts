a482fddf85781269562888992473eb665f40d310
Joel <raizetvision@gmail.com>
2025-05-28 02:30:56 +0200

    feat: chunks

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,28 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('/components/icons/')) {
+            return 'svg-icons'
+          }
+
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) return 'vue'
+            if (id.includes('echarts')) return 'echarts'
+            if (id.includes('pinia')) return 'pinia'
+            return 'vendor'
+          }
+        },
+      },
     },
   },
 })
